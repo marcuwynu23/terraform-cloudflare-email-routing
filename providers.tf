24ce0cac7c6ed668fd03795512016e68b836bd99
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-06-21 00:57:32 +0800

    feat: initial commit

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  email     = var.cloudflare_api_email != null ? var.cloudflare_api_email : null
+  api_key   = var.cloudflare_api_key != null ? var.cloudflare_api_key : null
+}
